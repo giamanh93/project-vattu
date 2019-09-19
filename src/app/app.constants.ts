1f4b9d07b41f883cf5124dad59cbc1e427499dc4
Gia Mạnh <manh.g.nguyen@vntrip.vn>
2019-09-19 13:35:22 +0700

    login

--- src/app/app.constants.ts
+++ src/app/app.constants.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class Configuration {
-    public server = 'http://quanlyvattu.com/';
+    public server = '/';
     public apiUrl = 'api/';
     public serverWithApiUrl = this.server + this.apiUrl;
 }
