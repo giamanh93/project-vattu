7faffea41cb5b085c75aac8f0657061e2c11956b
Gia Mạnh <manh.g.nguyen@vntrip.vn>
2019-09-20 22:03:02 +0700

    server error 401

--- src/app/core/helpers/authInterceptor.ts
+++ src/app/core/helpers/authInterceptor.ts
@@ -10,17 +10,14 @@ export class AuthInterceptor implements HttpInterceptor {
   constructor(private router: Router) { }
 
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-      if(request.body == null) {
-          this.router.navigate(['/login'])
-      }
     return next.handle(request)
       .pipe(
         catchError((err, caught: Observable<HttpEvent<any>>) => {
-            console.log(err)
-        //   if (err instanceof HttpErrorResponse && err.status == 401) {
-        //     this.router.navigate(['login'], { queryParams: { returnUrl: req.url } });
-        //     return of(err as any);
-        //   }
+          if (err instanceof HttpErrorResponse && err.status == 401) {
+            // this.router.navigate(['login'], { queryParams: { returnUrl: req.url } });
+            this.router.navigate(['/login']);
+            return of(err as any);
+          }
           throw err;
         })
       );
