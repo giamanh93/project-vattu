1f6d99b59d5122e370b45306677da4cf9e74a5e9
Gia Mạnh <manh.g.nguyen@vntrip.vn>
2019-09-20 21:44:28 +0700

    server error 401

--- src/app/app.constants.ts
+++ src/app/app.constants.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class Configuration {
-    public server = 'http://localhost:5000/';
+    public server = '/';
     public apiUrl = 'api/';
     public serverWithApiUrl = this.server + this.apiUrl;
 }
